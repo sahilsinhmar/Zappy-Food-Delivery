a954aae65ab1c07b472a93baccbc44638babfa32
Sahil <sahilsinhmar07@gmail.com>
2023-06-21 01:13:53 +0530

    added some extra value and added styles

--- src/components/RestraurantCard.js
+++ src/components/RestraurantCard.js
@@ -1,23 +1,50 @@
 import { url } from "../constant";
+import { AiFillStar } from "react-icons/ai";
 
 export const RestaurantCard = ({
   name,
   cuisines,
   cloudinaryImageId,
-  totalRatingsString,
+  costForTwoString,
+  slaString,
+  avgRating,
 }) => {
+  const buttonStyle = {
+    backgroundColor:
+      avgRating == "--"
+        ? "#fff"
+        : parseFloat(avgRating) < 4.0
+        ? "#db7c38"
+        : "#48c479",
+    color: isNaN(avgRating) ? "#535665" : "#fff",
+  };
   return (
-    <div className="card">
-      <div className="card-image">
-        <img src={url + cloudinaryImageId} />
+    <div className="flex-col w-60 mh-60 border rounded-lg  hover:scale-105  ease-in duration-300">
+      <div className="w-60 h-40 ">
+        <img
+          className="w-full h-full rounded object-cover"
+          src={url + cloudinaryImageId}
+        />
       </div>
-      <div className="cardinfo">
-        <div className="restro-name">
-          <span>{name}</span>
+      <div className="flex-col mt-1">
+        <div className="text-xl ml-1">
+          <h6>{name}</h6>
         </div>
-        <div className="restro-type">
-          <div>{cuisines.slice(0, 2).join(",")}</div>
-          <div>{totalRatingsString}</div>
+        <div className="text-xs ml-1">
+          <p>{cuisines.slice(0, 2).join(",")}</p>
+        </div>
+        <div className="flex justify-between items-center text-xs p-2 mt-1">
+          <div
+            className="flex items-center border justify-between gap-1 p-1"
+            style={buttonStyle}
+          >
+            <AiFillStar />
+            <span>{avgRating}</span>
+          </div>
+          <div>•</div>
+          <div>{slaString}</div>
+          <div>•</div>
+          <div>{costForTwoString}</div>
         </div>
       </div>
     </div>
