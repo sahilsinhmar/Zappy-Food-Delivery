43562b0e25d7094de0af5f62a9a8bc9144ec2bef
Sahil <96427085+sahilsinhmar@users.noreply.github.com>
2023-07-12 18:23:10 +0530

    Update helper.js

--- src/components/utils/helper.js
+++ src/components/utils/helper.js
@@ -2,6 +2,7 @@ export const FETCH_API_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4089123&lng=77.3177894&restaurantId=";
 
 export const GET_RESTAURANTS_LIST =
-  "https://instafood.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999";
+  "https://instafood.onrender.com/api/restaurants?lat=28.4089123&lng=77.3177894";
 export const GET_RESTAURANT_MENU =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4089123&lng=77.3177894&restaurantId=";
+  " https://instafood.onrender.com/api/menu?lat=28.4089123&lng=77.3177894&restaurantId=";
+
